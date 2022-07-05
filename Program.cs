bb78b1cf25b9060df744fddfb4f0c72a34dd3b2b
Simone Mauro <simon.mauro@virgilio.it>
2022-07-05 15:33:23 +0200

    Gestione eccezioni quasi ultimate

--- Program.cs
+++ Program.cs
@@ -11,16 +11,33 @@ while (!file.EndOfStream){
     string addresses = file.ReadLine(); //leggo l'intera riga del file
 
     string[] infoAddress = addresses.Split(","); //creo un array con gli elementi separati dalla ,
-    string name = infoAddress[0];
-    string surname = infoAddress[1];
-    string street = infoAddress[2];
-    string city = infoAddress[3];
-    string province = infoAddress[4];
-    string ZIP = infoAddress[5];
 
+    try{
+        string name = infoAddress[0];
+        string surname = infoAddress[1];
+        string street = infoAddress[2];
+        string city = infoAddress[3];
+        string province = infoAddress[4];
+        string ZIP = infoAddress[5];
+
+        if (infoAddress.Length > 6){
+            infoAddress[0] += infoAddress[1];
+            surname = infoAddress[2];
+            street = infoAddress[3];
+            city = infoAddress[4];
+            province = infoAddress[5];
+            ZIP = infoAddress[6];
+        }
     listaIndirizzi.Add(new Indirizzo(name, surname, street, city, province, ZIP));
+
+    }catch(IndexOutOfRangeException e){
+        Console.WriteLine(e.Message);
+    }
+
 }
 
 foreach (Indirizzo indirizzo in listaIndirizzi){
-    Console.WriteLine($"Stampa in corso: {indirizzo.Name}, {indirizzo.Surname}, {indirizzo.Street}, {indirizzo.City}, {indirizzo.Province}, {indirizzo.ZIP}");
+    Console.WriteLine($"Nome: {indirizzo.Name} Cognome: {indirizzo.Surname} Via: {indirizzo.Street} Città: {indirizzo.City} Provincia: {indirizzo.Province} Codice postale: {indirizzo.ZIP}");
 }
+
+file.Close();
