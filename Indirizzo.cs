b2cc2e392b39337734d8739ab80a1167e03f80ad
Simone Mauro <simon.mauro@virgilio.it>
2022-07-05 14:43:17 +0200

    Aggiungere i file di progetto.

---
+++ Indirizzo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_lista_indirizzi
+{
+    public class Indirizzo{
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string Province { get; set; }
+        public string ZIP { get; set; }
+
+        public Indirizzo(string name, string surname, string street, string city, string province, string zIP){
+            Name = name;
+            Surname = surname;
+            Street = street;
+            City = city;
+            Province = province;
+            ZIP = zIP;
+        }
+    }
+}
